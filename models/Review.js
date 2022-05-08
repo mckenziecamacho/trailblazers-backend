d3a9893b6d7c9c2cd71c9ae639c730682ec76be6
Boscobrand <Tony.Bosco@boscobrand.com>
2022-05-08 16:28:57 -0400

    Models created

---
+++ models/Review.js
@@ -0,0 +1,13 @@
+const mongoose = require("../db/connection");
+
+const reviewSchema = new mongoose.Schema (
+    {
+        title: String,   
+        rating: String,
+        body: String,
+        author: String
+    });
+
+const Review = mongoose.model("Review". reviewSchema)
+
+module.exports = Review;
---
+++ models/Trail.js
@@ -0,0 +1,21 @@
+const mongoose = require ("../db/connection");
+
+const trailSchema = new mongoose.Schema(
+    {
+        name: String,
+        location: String,
+        image: String,
+        difficulty: String,
+        miles: Number,
+        terrain: String,
+        duration: String,
+        overview: String,
+        website: String,
+        review: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    })
+
+const Trail = mongoose.model("Trail",trailSchema);
+module.exports = Trail
