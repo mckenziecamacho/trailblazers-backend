995e38092dfffdf9b1c4d6c8485620ce57cb3f6c
Boscobrand <Tony.Bosco@boscobrand.com>
2022-05-08 19:10:38 -0400

    seed collection 00 established

---
+++ db/seed/00 -- seed-collections.js
@@ -0,0 +1,49 @@
+const Trail = require('../../models/Trail');
+const Review = require('../../models/Review');
+
+const trailRawData = require('../data/trailRaw.json');
+const reviewRawData = require('../data/reviewRaw.json');
+
+const trailInfo = trailRawData.map(trailRawItem => {
+    const newTrail = {}
+    newTrail.name = trailRawItem.name
+    newTrail.location = trailRawItem.location
+    newTrail.image = trailRawItem.image
+    newTrail.difficulty = trailRawItem.difficulty
+    newTrail.miles = trailRawItem.miles
+    newTrail.terrain = trailRawItem.terrain
+    newTrail.duration = trailRawItem.duration
+    newTrail.overview = trailRawItem.overview
+    newTrail.website = trailRawItem.website
+    return newTrail;
+})
+
+Trail.deleteMany({})
+    .then(() => {
+        Trail.create(trailInfo)
+        .then(trails =>{
+            console.log(trails)
+        })
+        .catch(error =>{
+            console.log (error)
+        })
+    })
+
+const reviewInfo = reviewRawData.map(reviewRawItem => {
+    const newReview = {}
+    newReview.title = reviewRawItem.title
+    newReview.rating = reviewRawItem.rating
+    newReview.body = reviewRawItem.body
+    newReview.author = newReview.author
+})
+
+Review.deleteMany({})
+    .then(() => {
+        Review.create(reviewInfo)
+        .then(reviews => {
+            console.log(reviews)
+        })
+        .catch(error =>{
+            console.log(err)
+        })
+    })
