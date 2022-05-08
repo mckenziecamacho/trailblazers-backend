1bb394d5ae4d5dd360eaec77eee29b0ae809e0b0
Boscobrand <Tony.Bosco@boscobrand.com>
2022-05-08 19:53:58 -0400

    debugging index.js main and route

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 router.use("/", require("./application.js"));
-router.use("/api/trail", require("./trail.js"));
-router.use("/api/review", require("./review.js"));
+router.use("/api/trails", require("./trails.js"));
+router.use("/api/reviews", require("./reviews.js"));
 
 router.all("*", (req, res) => {
   res.status(400).send();
