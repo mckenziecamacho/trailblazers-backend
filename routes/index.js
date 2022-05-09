c2cfc5c52146479fa4efb9031b630161655d24ce
Boscobrand <Tony.Bosco@boscobrand.com>
2022-05-09 12:04:29 -0400

    post issue ticket 1 update (data and direction)

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 router.use("/", require("./application.js"));
-router.use("/api/trails", require("./trailRoute.js"));
-router.use("/api/reviews", require("./reviewRoute.js"));
+router.use("/api/trail", require("./trailRoute.js"));
+router.use("/api/review", require("./reviewRoute.js"));
 
-router.all("*", (req, res) => {
-  res.status(400).send();
-});
+// router.all("*", (req, res) => {
+//   res.status(400).send();
+// });
 
 module.exports = router;
