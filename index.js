9d387e7bb0d43c1d57db33d5755d2304edf9132a
Skills Academy <student03@MckenzieCamacho.fios-router.home>
2022-05-10 12:43:37 -0400

    cors

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ app.use(require("./routes/index.js"));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors());
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 
 app.set("port", process.env.PORT || 8080);
 
