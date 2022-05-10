0807094cac1c4ca2c921694690be1a572ce5647e
Skills Academy <student03@MckenzieCamacho.fios-router.home>
2022-05-10 14:32:28 -0400

    updates

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(require("./routes/index.js"));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors());
+
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
